8ba4c26a58dfdffb8d92dff9348ccaa504eaf392
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-12-26 16:49:08 -0300

    Traduzindo meses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        $locale = config('app.locale', 'pt_BR');
+
+        app()->setLocale($locale);
+        Carbon::setLocale($locale);
+
+        setlocale(LC_TIME, $locale . '.utf8', $locale . '.UTF-8', $locale);
     }
 }
