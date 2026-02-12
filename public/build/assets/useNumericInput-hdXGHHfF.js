dac8e97d610629c2c5eb2f1ceed5b1cf4f25add6
Danreby <rolimbernardo6@gmail.com>
2026-02-12 09:41:34 -0300

    Build

--- public/build/assets/useNumericInput-hdXGHHfF.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./app-BZa95d77.js";const t=["Backspace","Tab","ArrowLeft","ArrowRight","Delete","Home","End"],u=/^[0-9]$/;function s(){return r.useCallback(e=>{t.includes(e.key)||u.test(e.key)||e.preventDefault()},[])}function c(){return r.useCallback(e=>{t.includes(e.key)||e.key===","||e.key==="."||u.test(e.key)||e.preventDefault()},[])}export{c as a,s as u};
