ca67c387cb716eaaffc25aa71185245cbf07bb43
Danreby <rolimbernardo6@gmail.com>
2026-02-24 15:04:06 -0300

    Build

--- public/build/assets/useNumericInput-Bvr3k3Ke.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./app-CD5j2JfQ.js";const t=["Backspace","Tab","ArrowLeft","ArrowRight","Delete","Home","End"],u=/^[0-9]$/;function s(){return r.useCallback(e=>{t.includes(e.key)||u.test(e.key)||e.preventDefault()},[])}function c(){return r.useCallback(e=>{t.includes(e.key)||e.key===","||e.key==="."||u.test(e.key)||e.preventDefault()},[])}export{c as a,s as u};
