f3b0df47dafd03215bc5d1977f590e89a7a5a9ec
Bernardo Rolim <112499709+Danreby@users.noreply.github.com>
2026-03-01 17:11:45 -0300

    Build

---
+++ public/build/assets/BareButton-DuEJ1BaP.js
@@ -0,0 +1,1 @@
+import{j as u}from"./vendor-react--Ppq_H7a.js";function e({type:t="button",className:o="",children:n,...r}){return u.jsx("button",{type:t,className:o,...r,children:n})}export{e as B};
