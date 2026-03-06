5a7c6bc649e3833e04abe48d07e0e0ed3e0871bc
Danreby <rolimbernardo6@gmail.com>
2026-03-06 17:10:23 -0300

    build

--- public/build/assets/BareButton-ByoUOG-o.js
+++
@@ -1,1 +0,0 @@
-import{j as u}from"./vendor-CzyNov7S.js";function e({type:t="button",className:o="",children:n,...r}){return u.jsx("button",{type:t,className:o,...r,children:n})}export{e as B};
---
+++ public/build/assets/BareButton-C2Gp4Nhx.js
@@ -0,0 +1,1 @@
+import{j as u}from"./vendor-Z124E9Sp.js";function e({type:t="button",className:o="",children:n,...r}){return u.jsx("button",{type:t,className:o,...r,children:n})}export{e as B};
