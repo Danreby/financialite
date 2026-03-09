066a19d63fe0f8aee8589f7f1ff2b14079f3387e
Danreby <rolimbernardo6@gmail.com>
2026-03-09 09:54:40 -0300

    Build

--- public/build/assets/useNumericInput-BlP_fQwb.js
+++
@@ -1,1 +0,0 @@
-import{d as r}from"./vendor-C-6QEHyS.js";const t=["Backspace","Tab","ArrowLeft","ArrowRight","Delete","Home","End"],u=/^[0-9]$/;function s(){return r.useCallback(e=>{t.includes(e.key)||u.test(e.key)||e.preventDefault()},[])}function c(){return r.useCallback(e=>{t.includes(e.key)||e.key===","||e.key==="."||u.test(e.key)||e.preventDefault()},[])}export{c as a,s as u};
