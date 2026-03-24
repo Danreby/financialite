676de9e8c74f1d6df9c1b7e530e19051fb3c1cb9
Danreby <rolimbernardo6@gmail.com>
2026-03-24 11:52:59 -0300

    build

---
+++ public/build/assets/useNumericInput-C0A_EWmr.js
@@ -0,0 +1,1 @@
+import{e as r}from"./vendor-lPEHXF9F.js";const t=["Backspace","Tab","ArrowLeft","ArrowRight","Delete","Home","End"],u=/^[0-9]$/;function s(){return r.useCallback(e=>{t.includes(e.key)||u.test(e.key)||e.preventDefault()},[])}function c(){return r.useCallback(e=>{t.includes(e.key)||e.key===","||e.key==="."||u.test(e.key)||e.preventDefault()},[])}export{c as a,s as u};
