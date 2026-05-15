9e2dc043cff1e9bb522e5215342fc49dc924bec3
Danreby <rolimbernardo6@gmail.com>
2026-05-15 13:09:48 -0300

    Travando altura

--- resources/js/Components/system/dashboard/UpcomingBills.jsx
+++ resources/js/Components/system/dashboard/UpcomingBills.jsx
@@ -131,7 +131,7 @@ export default function UpcomingBills({
       </div>
 
       {unpaidBills.length > 0 ? (
-        <ScrollArea maxHeightClassName="" className="flex-1 min-h-0 -mx-6 px-6">
+        <ScrollArea maxHeightClassName="max-h-[490px]" className="flex-1 min-h-0 -mx-6 px-6">
           <div className="space-y-3">
             {unpaidBills.map((bill) => {
               const statusInfo = getStatusInfo(bill)
